5a5156464ee5067cd9306e8f4932906e3d90f893
Vlad Maranescu <vladmaranescu06@gmail.com>
2025-07-08 12:36:59 +0300

    added todays work and tutorial walktrough

---
+++ day1_tutorial.py
@@ -0,0 +1,59 @@
+from math import *
+print("Hello world")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+character_name="John"
+character_age = "35"
+is_male=False
+print("There was a man named " + character_name + ",")
+print("he was 70 years old.")
+character_name="Tom"
+print("He really liked the name "+character_name+",")
+print("giraffe\nAcademy")
+phrase ="Eu sunt un BAIAT"
+character_name="Vlad"
+print(phrase+" pe nume "+ character_name)
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[3])
+print(phrase.index("A"))
+print(phrase.replace("Eu","EU"))
+
+
+print(-2.4+4.8*2 )
+print(10%3)
+my_num = -4
+print(my_num)
+print(str(my_num)+"my fav nmbr")
+print(abs(my_num))
+print(pow(2,10))
+print(max(3,5))
+print(round(3.5))
+print(floor(3.256))
+print(ceil(3.256))
+print(sqrt(256))
+
+name = input("Enter name:")
+age = input("Enter age:")
+print("Hello "+name+" i am "+character_name+age+" is a nice age,but this age is much more nice "+character_age)
+
+
+num_1 = input("Enter your first number:")
+num_2 =input("Enter your second number:")
+result=float(num_1)+float(num_2)
+print(result)
+
+
+
+
+color = input("enter a color name :")
+plural_noun = input("enter a plural noun:")
+celebrity =input("enter a celebritys name:")
+print("Roses \n are "+ color)
+print(plural_noun + " \n are blue ")
+print("I love " + celebrity)
